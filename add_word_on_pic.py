4754aa960548b2d94f7aef8611e7c557fbd24885
zrt0317 <zrt0317@pku.edu.cn>
2020-03-28 22:58:53 +0800

    Create add_word_on_pic.py

---
+++ add_word_on_pic.py
@@ -0,0 +1,23 @@
+import glob
+c = glob.glob(r"""D:\temp\*_width==1024.jpg""")
+
+
+import cv2
+from PIL import Image
+
+for i in range(len(c)):
+    # load picture
+    bk_img = cv2.imread(c[i])
+    
+    # add rectangle and word
+    width, height, _ = bk_img.shape
+    cv2.rectangle(bk_img, (height-100, width-50), (height, width), (0, 0, 0), -1)
+    cv2.putText(bk_img,
+                (str(i).rjust(3,'0')),
+                (height-70, width-20),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.7,
+                (255,255,255),
+                1,
+                cv2.LINE_AA)
+    cv2.imwrite((str(i).rjust(3,'0'))+".jpg", bk_img)
